9808bef0d8266059238787d1cf33b0c68dba1235
haoxuan.wu <haoxuan.wu@neulion.com.cn>
2016-12-23 17:28:49 +0800

    modify public

--- callsports/service/publish.php
+++ callsports/service/publish.php
@@ -2,6 +2,17 @@
 require_once('../library/SQLManager.php');
 use callsports\library\SQLManager as SQLManager;
 
+
+function create_uuid($prefix = ""){    //可以指定前缀
+    $str = md5(uniqid(mt_rand(), true));   
+    $uuid  = substr($str,0,8) . '-';   
+    $uuid .= substr($str,8,4) . '-';   
+    $uuid .= substr($str,12,4) . '-';   
+    $uuid .= substr($str,16,4) . '-';   
+    $uuid .= substr($str,20,12);   
+    return $prefix . $uuid;
+}
+
 try{
 
 	//get the publish message
@@ -23,35 +34,37 @@ try{
 
    	$tableDesc = array("id varchar(50) not null primary key "
    						,"user_id varchar(30) not null"
-   						,"name varchar(40)  not null "
+   						,"name varchar(40) not null"
    						,"time varchar(30) not null"
-   						,"address  text  not null"
+   						,"address  text not null"
               ,"latitude double not null"
               ,"longitude double not null"
    						,"total_num varchar(30) not null"
    						,"now_num varchar(30) not null"
    						,"cost varchar(20) not null"
-   						,"introduce text  not null");
+   						,"introduce text");
 
   	$result = $sqlM->createTable($tableDesc);
 
   	if(!$result)
         throw new Exception("cteateTable  all_activities error");
 
-
+  $uuid=create_uuid();
   //insert to all_activities
     $result = $sqlM->insertValue(array(id,user_id,name,time,address,latitude,longitude,total_num,now_num,cost,introduce)
-    							,array("'".$id."'","'".$user_id."'","'".$name."'","'".$time."'", "'".$address."'","'".$latitude."'","'".$longitude."'","'".$total_num."'","'".$now_num."'","'".$cost."'" ,"'".$introduce."'"));
-  
+    							,array("\"$uuid\"","'".$user_id."'","'".$name."'","'".$time."'", "'".$address."'","'".$latitude."'","'".$longitude."'","'".$total_num."'","'".$now_num."'","'".$cost."'" ,"'".$introduce."'"));
+    
   
   if (!$result) {
-    throw new Exception('Could not insert your activity in database - please try again later.');
+   echo json_encode(array("result"=>'Could not insert your activity in database - please try again later.'));
   }
+
+   // $resultSignal=$sqlM->insertValue(array(id,user_id,activity,type),array());
 
   echo json_encode(array('result' => 'success'));      
 
 }catch(Exception  $e){
-	  $errInfo = $e->getMessage();
+	   $errInfo = $e->getMessage();
       echo json_encode(array('result' => $errInfo));      
       exit;
 }
