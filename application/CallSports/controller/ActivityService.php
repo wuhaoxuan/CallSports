cf957cd64248cf1065d0479c8fc7dac4eb88ff19
haoxuan.wu <haoxuan.wu@neulion.com.cn>
2017-01-19 22:25:48 +0800

    modify create activity

--- application/CallSports/controller/ActivityService.php
+++ application/CallSports/controller/ActivityService.php
@@ -1,15 +1,24 @@
 <?php
 namespace app\CallSports\controller;
 use app\CallSports\model\ActivityModel;
+use think\Db;
 
 class ActivityService
 {
 
     public function publish($user_id,$name,$time,$address,$latitude,$longitude,$total_num,$cost,$introduce)
     {
+        $this->createAllActivitiesTable();
         $activityModel=new ActivityModel();
         $result=$activityModel->publish($user_id,$name,$time,$address,$latitude,$longitude,$total_num,$cost,$introduce);
         return $result;
+    }
+
+
+    private function createAllActivitiesTable()
+    {
+        $createBtn="create table IF NOT EXISTS all_activities(user_id VARCHAR(30),uuid TEXT,name TEXT,time VARCHAR(30),address TEXT,latitude DOUBLE,longitude DOUBLE,total_num INT ,cost VARCHAR(10),introduce TEXT,now_num INT,members TEXT)";
+        Db::execute($createBtn);
     }
 
     public function joinAct($creater_id,$user_id,$uuid)
--- application/CallSports/model/AccountModel.php
+++ application/CallSports/model/AccountModel.php
@@ -68,7 +68,7 @@
           $result=self::where(['user_id'=>$userId,'password'=>$shaPassword])->find();
           if(!empty($result))
           {
-              return ['result'=>'success'];
+              return ['result'=>'success','user_id'=>$userId];
           }
           else
           {
